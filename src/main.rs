431344f13d10ed20315039c102e34ebe0d799e4f
HosokawaR <thin.river.jp@gmail.com>
2022-01-22 11:53:30 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+fn do_work(inp: &str, out: Option<String>) {
+    println!("{}", inp);
+    match out {
+        Some(x) => println!("{}", x),
+        None => println!("No Output"),
+    }
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} PATH [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("j", "json", "output result as json");
+    opts.optflag("h", "help", "HELP HELP HELP");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!("{}", f)
+        }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let output = matches.opt_str("j");
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+    do_work(&input, output);
 }
