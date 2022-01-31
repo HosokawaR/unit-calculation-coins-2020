814866d01326dbe671e94d9919235b8aa828d5f4
HosokawaR <thin.river.jp@gmail.com>
2022-01-31 17:34:31 +0900

    csv の読み込み機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,9 @@
 extern crate getopts;
 use getopts::Options;
-use serde::Deserialize;
 use std::env;
-use std::error::Error;
 
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[serde(rename = "学籍番号")]
-    student_number: String,
-    #[serde(rename = "学生氏名")]
-    student_name: String,
-    #[serde(rename = "科目番号")]
-    course_code: String,
-    #[serde(rename = "科目名")]
-    course_name: String,
-    // csv の単位数カラムに余分な空白があり serde の標準機能では f64 に変換できないため独自の変換関数を用意
-    #[serde(rename = "単位数", deserialize_with = "deserialize_credit")]
-    credit: f64,
-    #[serde(rename = "春学期")]
-    spring: String,
-    #[serde(rename = "秋学期")]
-    fall: String,
-    #[serde(rename = "総合評価")]
-    overall_grade: String,
-    #[serde(rename = "科目区分")]
-    course_category: String,
-    #[serde(rename = "開講年度")]
-    semester_year: u32,
-    #[serde(rename = "開講区分")]
-    semester_status: String,
-}
-
-fn deserialize_credit<'de, D>(deserializer: D) -> Result<f64, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let credit: &str = Deserialize::deserialize(deserializer)?;
-
-    let credit = credit.trim();
-
-    if credit.is_empty() {
-        return Ok(0.0);
-    }
-
-    let credit = credit.parse::<f64>().map_err(serde::de::Error::custom)?;
-
-    Ok(credit)
-}
-
-fn calc(input_path: &str, output_path: Option<String>) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(input_path)?;
-
-    for result in reader.deserialize() {
-        let record: Record = result.expect("Reading CSV failed");
-        println!("{:?}", record);
-    }
-
-    match output_path {
-        Some(path) => {
-            println!("Writing CSV to {}", path);
-        }
-        None => {}
-    }
-
-    Ok(())
-}
+mod judgement;
+mod read_csv;
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} CSV_PATH [options]", program);
@@ -100,5 +39,7 @@ fn main() {
         return;
     };
 
-    calc(&input_path, output_path);
+    let mut records = read_csv::read_csv(input_path.as_str()).expect("Failed to read csv");
+
+    println!("{:?}", records);
 }
---
+++ src/read_csv.rs
@@ -0,0 +1,63 @@
+extern crate getopts;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    #[serde(rename = "学籍番号")]
+    pub student_number: String,
+    #[serde(rename = "学生氏名")]
+    pub student_name: String,
+    #[serde(rename = "科目番号")]
+    pub course_code: String,
+    #[serde(rename = "科目名")]
+    pub course_name: String,
+    // csv の単位数カラムに余分な空白があり serde の標準機能では f64 に変換できないため独自の変換関数を用意
+    #[serde(rename = "単位数", deserialize_with = "deserialize_credit")]
+    pub credit: f64,
+    #[serde(rename = "春学期")]
+    pub spring: String,
+    #[serde(rename = "秋学期")]
+    pub fall: String,
+    #[serde(rename = "総合評価")]
+    pub overall_grade: String,
+    #[serde(rename = "科目区分")]
+    pub course_category: String,
+    #[serde(rename = "開講年度")]
+    pub semester_year: u32,
+    #[serde(rename = "開講区分")]
+    pub semester_status: String,
+    #[serde(default = "bool::default")]
+    pub read: bool,
+}
+
+impl Record {
+    pub fn is_read(&self) -> bool {
+        self.read
+    }
+
+    pub fn set_read(&mut self, read: bool) {
+        self.read = read;
+    }
+}
+
+fn deserialize_credit<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let credit: &str = Deserialize::deserialize(deserializer)?;
+
+    let credit = credit.trim();
+
+    if credit.is_empty() {
+        return Ok(0.0);
+    }
+
+    let credit = credit.parse::<f64>().map_err(serde::de::Error::custom)?;
+
+    Ok(credit)
+}
+
+pub fn read_csv(input_path: &str) -> Result<Vec<Record>, csv::Error> {
+    let reader = csv::Reader::from_path(input_path)?;
+    reader.into_deserialize().collect()
+}
