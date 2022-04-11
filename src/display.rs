cf8f1e9e8296f2f091f566b59b0f43c2027623ca
HosokawaR <thin.river.jp@gmail.com>
2022-04-11 21:00:26 +0900

    WIP

--- src/display.rs
+++ src/display.rs
@@ -1,7 +1,7 @@
 use super::judgement::Requirement;
 use colored::Colorize;
 
-pub fn display_result(requirements: &Requirement, prefix: &str, children_prefix: &str) {
+pub fn display_result(requirements: &Requirement, prefix: &str, children_prefix: &str, pat: &str) {
     print!("{}{} ", prefix, requirements.label);
 
     let result =
@@ -12,6 +12,12 @@ pub fn display_result(requirements: &Requirement, prefix: &str, children_prefix:
         false => println!("{}", result.red()),
     }
 
+    if prefix.len() >= 4 {
+        println!("{}    {}", &prefix.split_at(prefix.len() - 4).0, "test");
+    } else {
+        println!("{}    {}", &prefix, "test");
+    }
+
     let mut iter = requirements.children.iter().peekable();
 
     while let Some(child) = iter.next() {
@@ -19,12 +25,22 @@ pub fn display_result(requirements: &Requirement, prefix: &str, children_prefix:
             true => {
                 let child_prefix = format!("{}{}", children_prefix, "├── ");
                 let child_children_prefix = format!("{}{}", children_prefix, "│   ");
-                display_result(child, &child_prefix, &child_children_prefix);
+                display_result(
+                    child,
+                    &child_prefix,
+                    &child_children_prefix,
+                    &children_prefix,
+                );
             }
             false => {
                 let child_prefix = format!("{}{}", children_prefix, "└── ");
                 let child_children_prefix = format!("{}{}", children_prefix, "    ");
-                display_result(child, &child_prefix, &child_children_prefix);
+                display_result(
+                    child,
+                    &child_prefix,
+                    &child_children_prefix,
+                    &children_prefix,
+                );
             }
         }
     }
